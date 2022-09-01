b6b95812cd401982a42d92b8087cdd016ab60616
Daniel Medina <luis.medina.aguilera21@gmail.com>
2022-09-01 12:23:47 -0400

    change database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_08_27_170720) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bitcoins", force: :cascade do |t|
     t.string "block_hash"
     t.string "prev_block"
